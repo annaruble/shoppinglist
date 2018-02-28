680b12269a87a335ff02208f7553496b0229c413
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2018-02-28 16:30:33 -0600

    resolving bug fix, add class outlines

--- backend.js
+++ backend.js
@@ -1,5 +1,24 @@
 "use strict;"
 
+// class Item {
+//     constructor(itemC, qtyC, prtyC, storeC, sectionC, priceC) {
+//         this.itemC = itemC;
+//         this.qtyC = qtyC;
+//         this.prtyC = prtyC;
+//         this.storeC = storeC;
+//         this.sectionC = sectionC;
+//         this.priceC = priceC;
+//     }
+// }
+
+// class ShoppingList {
+//     constructor(list) {
+//         this.list = list;
+//     }
+
+//     list = Array.from(Item)
+// }
+
 //function search(){}
 
 function addNew() {
@@ -7,7 +26,7 @@ function addNew() {
 
     let row = table.insertRow(1);
 
-    //let check = row.insertCell(0);
+    //let check = row.insertCell(-1);
     let item = row.insertCell(0);
     let qty = row.insertCell(1);
     let priority = row.insertCell(2);
@@ -15,7 +34,10 @@ function addNew() {
     let section = row.insertCell(4);
     let price = row.insertCell(5);
 
-    //check.innerHTML = document.createElement("input").type("checkbox");
+    //let checkbox = document.createElement('input');
+    //checkbox.type("checkbox");
+
+    //check.appendChild(checkbox);
     item.innerHTML = document.getElementById("itemIn").value;
     qty.innerHTML = document.getElementById("qtyIn").value;
     priority.innerHTML = document.getElementById("priorityIn").value;

16c7936850ff18462d26e3505aca94d5887baef1
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2018-03-08 08:53:49 -0600

    fixing checkboxes

--- backend.js
+++ backend.js
@@ -5,21 +5,22 @@
 
 function addNew() {
     let table = document.getElementById("tableList");
-
+    //let row = document.createElement("tr")
     let row = table.insertRow(1);
 
-    let check = row.insertCell(-1);
-    let item = row.insertCell(0);
-    let qty = row.insertCell(1);
-    let priority = row.insertCell(2);
-    let store = row.insertCell(3);
-    let section = row.insertCell(4);
-    let price = row.insertCell(5);
+    let check = row.insertCell(0);
+    let item = row.insertCell(1);
+    let qty = row.insertCell(2);
+    let priority = row.insertCell(3);
+    let store = row.insertCell(4);
+    let section = row.insertCell(5);
+    let price = row.insertCell(6);
 
-    //let checkbox = document.createElement('input').appendChild;
-    //checkbox.type("checkbox");
+    let checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
 
-    //check.appendChild(checkbox);
+    check.appendChild(checkbox);
+    //check.innerHTML = "Hello"
     item.innerHTML = document.getElementById("itemIn").value;
     qty.innerHTML = document.getElementById("qtyIn").value;
     priority.innerHTML = document.getElementById("priorityIn").value;
